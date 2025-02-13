04a2a051cbee51153335fcbf4d2288154fb4d8a4
Trayan <trayanshterionov7@gmail.com>
2025-02-13 20:32:39 +0200

    image loading

--- CPP_chess/Chess/Chess.cpp
+++ CPP_chess/Chess/Chess.cpp
@@ -6,22 +6,23 @@ int main()
 
     const int screen_width = 840;
     const int screen_height = 840;
+
     const int square_size = 100;
     const int board_offset = 40;
 
     InitWindow(screen_width,screen_height, "Chess");
     SetTargetFPS(60);
-
-    Texture2D pawnTexture = LoadTexture("Assets/chess_king.png");    
+    Image pawn = LoadImage("Assets/chess_king.png");
+    Texture2D pawnTexture = LoadTextureFromImage(pawn);    
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(BLACK);
         DrawTexture(pawnTexture,200,200,WHITE);
         
+        
         for (int row = 0; row < 8; row++)
-        {
-            for (int col = 0; col < 8; col++)
+        { for (int col = 0; col < 8; col++)
             {
                 int x = row * square_size + board_offset;
                 int y = col * square_size + board_offset;
@@ -38,10 +39,13 @@ int main()
             
         }
         
-        UnloadTexture(pawnTexture);
-      //  int testY = board_offset + 3 * square_size;
-     //   int testX = board_offset + 4 * square_size;
-     //   whitePawn.Draw(testX,testY,square_size);
+        
+         int testY = board_offset + square_size;
+         int testX = board_offset + 4 * square_size;
+         Rectangle destination{board_offset + column,testY,float{square_size},float{square_size}};
+         Rectangle source{0,0,float{pawn.width},float{pawn.height}};
+         Vector2 origin{square_size / 2.0f,square_size / 2.0f};
+        DrawTexturePro(pawnTexture,source,destination,origin,0.0f,WHITE);
         
         EndDrawing();
     }
